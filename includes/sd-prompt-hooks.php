19367cb6408f0c962f503c20c2cdd44418b38665
pshaung <speshiou@gmail.com>
2023-12-08 09:30:13 +0800

    Wrap slugs with category links instead tag links

--- includes/sd-prompt-hooks.php
+++ includes/sd-prompt-hooks.php
@@ -3,25 +3,34 @@
 function pl_sd_prompt_content( $content ) {
     global $post;
 
-    if ( !in_category( 'sd-prompt', $post ) ) {
+    if ( !is_sd_prompt( $post ) ) {
         return $content;
     }
 
-    $category = get_term_by('slug', 'sd-prompt', 'category');
-    $cat_link = get_category_link($category->term_id);
+    $cat_sd_prompt = get_term_by('slug', 'sd-prompt', 'category');
+    $args = [
+        'parent' => $cat_sd_prompt->term_id,
+        'hide_empty' => false,
+    ];
+    $categories = get_categories( $args );
+    $category_map = array_reduce($categories, function($carry, $item) {
+        $carry[$item->slug] = $item;
+        return $carry;
+    }, []);
 
     $content = sd_prompt_content($post, $content);
 
-    // add concept filter link
-    $content = preg_replace_callback("/💡 concept/i", function($matches) use ($cat_link) {
-        $link = add_query_arg( 'filter', 'concept', $cat_link );
+    // replace category links
+    $content = preg_replace_callback("/💡 concept/i", function($matches) use ($category_map) {
+        $link = array_key_exists('concept', $category_map) ? get_category_link($category_map['concept']) : '';
         return sprintf('💡 <a href="%s">concept</a>', esc_url($link));
     }, $content);
 
-    // add tag filter links
-    $content = preg_replace_callback("/([^<>]+) (-|\&#8211;) ([^<>]+)/i", function($matches) use ($cat_link) {
-        $link = add_query_arg( 'filter', $matches[1], $cat_link );
-        return sprintf('<a href="%s">%s</a> &#8211; %s', $link, $matches[1], $matches[3]);
+    // replace category links
+    $content = preg_replace_callback("/([^<>]+) (-|\&#8211;) ([^<>]+)/i", function($matches) use ($category_map) {
+        $slug = $matches[1];
+        $link = array_key_exists($slug, $category_map) ? get_category_link($category_map[$slug]) : '';
+        return sprintf('<a href="%s">%s</a> &#8211; %s', $link, $slug, $matches[3]);
     }, $content);
 
     return $content;
